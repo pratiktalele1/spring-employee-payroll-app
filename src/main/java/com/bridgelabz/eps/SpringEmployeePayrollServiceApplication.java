396b1311393f468c9ae84ab6105e88bca67e829f
pratiktalele1 <pratiktalele1@gmail.com>
2021-09-18 15:07:06 +0530

    logging environment is added

--- src/main/java/com/bridgelabz/eps/SpringEmployeePayrollServiceApplication.java
+++ src/main/java/com/bridgelabz/eps/SpringEmployeePayrollServiceApplication.java
@@ -2,14 +2,20 @@ package com.bridgelabz.eps;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
-@SpringBootApplication
+import lombok.extern.slf4j.Slf4j;
+
+@SpringBootApplication @Slf4j
 public class SpringEmployeePayrollServiceApplication {
 
 	public static void main(String[] args) {
 		
-		SpringApplication.run(SpringEmployeePayrollServiceApplication.class, args);
+		ApplicationContext applicationContext= SpringApplication.run(SpringEmployeePayrollServiceApplication.class, args);
 		System.out.println("hello from employee payroll");
+		System.out.println("environment is ->"+applicationContext.getEnvironment());
+		log.debug("environment is ->"+applicationContext.getEnvironment());
+
 	}
 
 }
--- src/main/java/com/bridgelabz/eps/controller/EmployeePayrollController.java
+++ src/main/java/com/bridgelabz/eps/controller/EmployeePayrollController.java
@@ -22,9 +22,12 @@ import com.bridgelabz.eps.model.EmployeeData;
 import com.bridgelabz.eps.model.EmployeePayroll;
 import com.bridgelabz.eps.service.IEmployeePayroll;
 
+import lombok.extern.slf4j.Slf4j;
+
 
 @RestController
 @RequestMapping("/service")
+@Slf4j
 public class EmployeePayrollController {
 
 //	IEmployeePayroll is Autowired to call various functions from service layer
@@ -40,30 +43,35 @@ public class EmployeePayrollController {
 //	getAll() is created to get all payroll data from service layer using http get method
 	@GetMapping
 	private ResponseEntity<List<EmployeeData>> getAll() {
+		log.info("In the getAll() function to retrive all data");
 		return new ResponseEntity<>(services.readAll(), HttpStatus.OK);
 	}
 
 //	create() is created to save payroll data to service layer using http post method
 	@PostMapping
 	private ResponseEntity<ResponseDTO> create(@RequestBody EmployeePayrollDTO e) {
+		log.info("In the create() function to create data");
 		return new ResponseEntity<>(services.createEmployee(e), HttpStatus.CREATED);
 	}
 
 //	getById() is created to get payroll data by id from service layer using http get method
 	@GetMapping("/{id}")
 	private ResponseEntity<ResponseDTO> getById(@PathVariable int id) {
+		log.info("In the getById() function to retrive data by using Id");
 		return new ResponseEntity<>(services.getDataById(id), HttpStatus.FOUND);
 	}
 
 //	updateById() is created to update payroll data by id from service layer using http put method
 	@PutMapping("/{id}")
 	private ResponseEntity<ResponseDTO> updateById(@PathVariable int id, @RequestBody EmployeePayrollDTO dto) {
+		log.info("In the updateById() function to update data by using Id");
 		return new ResponseEntity<>(services.updateDataById(id,dto), HttpStatus.FOUND);
 	}
 
 //	deleteById() is created to delete payroll data by id from service layer using http delete method
 	@DeleteMapping("/{id}")
 	private ResponseEntity<ResponseDTO> deleteById(@PathVariable int id) {
+		log.info("In the deleteById() function to delete data by using Id");
 		return new ResponseEntity<>(services.deleteDataById(id), HttpStatus.FOUND);
 	
 	}
