ad9fdca551ef5022ec3309af3206d28362a7dbd5
pratiktalele1 <pratiktalele1@gmail.com>
2021-09-20 23:05:21 +0530

    Exception is handeled using Method argument not valid class

--- src/main/java/com/bridgelabz/eps/exceptionhandler/InputException.java
+++ src/main/java/com/bridgelabz/eps/exceptionhandler/InputException.java
@@ -1,6 +1,7 @@
 package com.bridgelabz.eps.exceptionhandler;
 
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 
 @Component
 public class InputException extends RuntimeException {
